6472793a0cb52e3eed3961e1304d17439298a029
Fabricio <101414179+FaArMa@users.noreply.github.com>
2023-05-24 18:36:34 -0300

    Initial Screen: Refactorizado para utilizar Database

--- SGG/scenes/initial_screen/initial_screen.gd
+++ SGG/scenes/initial_screen/initial_screen.gd
@@ -1,18 +1,57 @@
 extends Control
 
 
+@onready var DB = Database.new()
+@onready var login_button = $Login
+@onready var register_button = $Register
+@onready var error_label = $Incorrect
+
+
+# Se ejecuta cuando el nodo entra en el árbol de escena por primera vez
 func _ready():
+	# Agregar DB como nodo hijo de InitialScreen para luego conectar con sus señales
+	add_child(DB)
+	DB.connect("response_users_count", _on_get_users_count)
+	DB.connect("response_error", _on_response_error)
+	# ¿Hay usuarios? entonces vas a "Iniciar sesión"
+	# ¿No hay usuarios? entonces sos el primero (automaticamente Dueño) y vas a "Registrarte"
+	# ¿Hubo un error? F en el chat
+	DB.get_users_count()
 	# Al darle el foco se puede manejar con teclado sin necesidad de hacer clic antes
-	$Login.grab_focus()
+	login_button.grab_focus()
 
 
+# Se ejecuta cuando se presiona el boton Login
 func _on_login_pressed():
 	get_tree().change_scene_to_file("res://scenes/login_screen/login_screen.tscn")
 
 
+# Se ejecuta cuando se presiona el boton Register
 func _on_register_pressed():
-	pass # TODO Crear la escena de registro de usuario
+	get_tree().change_scene_to_file("res://scenes/register_screen/register_screen.tscn")
 
 
+# Se ejecuta cuando se presiona el boton Quit
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+# Se ejecuta cuando Database envia su señal de get_users_count
+func _on_get_users_count(count):
+	if (!count.is_valid_int() || count.to_int() < 0):
+		print("[ERROR] La cantidad de usuarios registrados no es un número válido: %s" % count)
+		error_label.text = "La cantidad de usuarios registrados no es un número válido"
+		error_label.visible = true
+		return
+	print("[OK] La cantidad de usuarios registrados es: %s" % count)
+	if (count.to_int() > 0):
+		login_button.disabled = false
+	else:
+		register_button.disabled = false
+
+
+# Se ejecuta cuando Database envia su señal de response_error
+func _on_response_error(error_msg):
+	error_label.text = error_msg
+	error_label.visible = true
+
