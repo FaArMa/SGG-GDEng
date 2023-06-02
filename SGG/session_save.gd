7a7d0fa9812937a0b37c8cb264b539a1bf8483c3
iglop <nacholopo@gmail.com>
2023-06-02 15:08:10 -0300

    Guardado y cargado de mesas y paredes.

--- SGG/session_save.gd
+++ SGG/session_save.gd
@@ -1,22 +1,27 @@
 class_name SessionSave
 extends Node
 
-const SAVE_PATH := "user://session_save.save"
-var stored_table_positions: Dictionary = {}
+const SAVE_PATH := "user://session_save.cfg"
 
-func save_state(_table_positions: Dictionary):
-	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
-	file.store_var(_table_positions)
-	stored_table_positions = _table_positions
-	file.close()
-	print("- Ubicación de Mesas Guardada -")
 
-func load_state(_table_positions: Dictionary):
-	if not FileAccess.file_exists(SAVE_PATH):
-		print("- Archivo No Encontrado -")
-		save_state(_table_positions)
-	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
-	stored_table_positions = file.get_var()
-	file.close()
-	print("- Ubicación de Mesas Cargada -")
-	return stored_table_positions
+func save_state(tables: Dictionary, walls: Array):
+	var config = ConfigFile.new()
+
+	config.set_value("Objects", "Tables", tables)
+	config.set_value("Objects", "Walls", walls)
+
+	config.save(SAVE_PATH)
+	print("- Objetos Guardados -")
+
+
+func load_state(tables: Dictionary, walls: Array):
+	var config = ConfigFile.new()
+
+	var err = config.load(SAVE_PATH)
+
+	if err != OK:
+		print("- Error Cargando Objetos -")
+		save_state(tables, walls)
+
+	return [config.get_value("Objects", "Tables", tables), config.get_value("Objects", "Walls", walls)]
+
