70d943a878d613e5a61e1d852715a7fbb528c6f8
Fabricio <101414179+FaArMa@users.noreply.github.com>
2023-07-05 14:55:11 -0300

    Billing Menu: Refactorizar la selección y llenado de fechas

--- SGG/scenes/billing_menu_scene/billing_menu_scene.gd
+++ SGG/scenes/billing_menu_scene/billing_menu_scene.gd
@@ -1,5 +1,6 @@
 extends Control
 
+
 @onready var Search_Dialog = $Billing_Menu_Container/Search_Result_Container
 @onready var Search_Items = $Billing_Menu_Container/Search_Result_Container/Search_Result_Content_Container/Search_Container/Search_Items
 @onready var Search_Summary = $Billing_Menu_Container/Search_Result_Container/Search_Result_Content_Container/Search_Summary
@@ -9,35 +10,31 @@ extends Control
 @onready var To_Day = $Billing_Menu_Container/Content_Container/Input_And_Button_Container/To_Date_Container/To_Day
 @onready var To_Month = $Billing_Menu_Container/Content_Container/Input_And_Button_Container/To_Date_Container/To_Month
 @onready var To_Year = $Billing_Menu_Container/Content_Container/Input_And_Button_Container/To_Date_Container/To_Year
-
 @onready var bills_in_search: Dictionary = {}
+@onready var system_datetime: Dictionary = Time.get_datetime_dict_from_system()
 
 
 func _ready():
 	EventBus.connect("billing_list_updated", _on_billing_update)
 	fill_dates()
 
-	
+
 func fill_dates():
-	for i in range(1,32):
-		if i < 10:
-			From_Day.add_item("0%d" % i)
-			To_Day.add_item("0%d" % i)
-		else:
-			From_Day.add_item("%d" % i)
-			To_Day.add_item("%d" % i)
+	for i in range(1, 32):
+		From_Day.add_item("%02d" % i)
+		To_Day.add_item("%02d" % i)
 
 	for i in range(1, 13):
-		if i < 10:
-			From_Month.add_item("0%d" % i)
-			To_Month.add_item("0%d" % i)
-		else:
-			From_Month.add_item("%d" % i)
-			To_Month.add_item("%d" % i)
+		From_Month.add_item("%02d" % i)
+		To_Month.add_item("%02d" % i)
 
 	for i in range(2020, 2024):
 		From_Year.add_item("%d" % i)
 		To_Year.add_item("%d" % i)
+
+	From_Day.selected = system_datetime.day - 1
+	From_Month.selected = system_datetime.month - 1
+	From_Year.selected = system_datetime.year - From_Year.get_item_text(0).to_int()
 
 	To_Day.selected = -1
 	To_Month.selected = -1
@@ -67,7 +64,7 @@ func _on_accept_button_pressed():
 
 func _on_billing_update():
 	bills_in_search = DatabaseContent.bill_list
-		
+
 	var total_amount = 0
 
 	for i in bills_in_search.keys():
@@ -86,6 +83,9 @@ func _on_billing_update():
 func _on_exit_search_pressed():
 	Search_Items.clear()
 	Search_Dialog.hide()
+	From_Day.selected = system_datetime.day - 1
+	From_Month.selected = system_datetime.month - 1
+	From_Year.selected = system_datetime.year - From_Year.get_item_text(0).to_int()
 	To_Day.selected = -1
 	To_Month.selected = -1
 	To_Year.selected = -1
@@ -105,9 +105,9 @@ func match_month_length(input: OptionButton, index: int):
 	match index:
 		1:
 			length = 29
-		3,5,8,10:
+		3, 5, 8, 10:
 			length = 31
-		0,2,4,6,7,9,11:
+		0, 2, 4, 6, 7, 9, 11:
 			length = 32
 
 	correct_days(input, length, index)
@@ -117,13 +117,8 @@ func correct_days(input, month_length, index):
 	var last_selected_index = input.get_selected()
 	input.clear()
 
-	for i in range(1,month_length):
-		if i < 10:
-			input.add_item("0%d" % i)
-		else:
-			input.add_item("%d" % i)
+	for i in range(1, month_length):
+		input.add_item("%02d" % i)
 
 	if index < month_length:
 		input.selected = last_selected_index
-
-
